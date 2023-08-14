f4777e96c5b3a8be41b0c0439ccd76ce938e0f1e
yogakrishanthi <yogeswarandharsha@gmail.com>
2023-08-14 13:06:53 +0530

    update

--- client/src/components/reserve/Reserve.js
+++ client/src/components/reserve/Reserve.js
@@ -10,7 +10,7 @@ import { useNavigate } from "react-router-dom";
 
 const Reserve = ({ setOpen, hotelId }) => {
   const [selectedRooms, setSelectedRooms] = useState([]);
-  const { data, loading, error } = useFetch(`https://project-crud.onrender.com/hotels/room/${hotelId}`);
+  const { data, loading, error } = useFetch(`https://project-crud.onrender.com/api/hotels/room/${hotelId}`);
   const { dates } = useContext(SearchContext);
 
   const getDatesInRange = (startDate, endDate) => {
@@ -55,7 +55,7 @@ const Reserve = ({ setOpen, hotelId }) => {
     try {
       await Promise.all(
         selectedRooms.map((roomId) => {
-          const res = axios.put(`https://project-crud.onrender.com/rooms/availability/${roomId}`, {
+          const res = axios.put(`https://project-crud.onrender.com/api/rooms/availability/${roomId}`, {
             dates: alldates,
           });
           return res.data;
